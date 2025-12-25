7d44a65384ad885a523fec3a80319e7783b9bef6
manbin <2766045970@qq.com>
2025-12-25 16:02:58 +0800

    rvcp: init RegAlloc Pass

--- rvcp/src/rv/RegAlloc.cpp
+++ rvcp/src/rv/RegAlloc.cpp
@@ -2,11 +2,73 @@
 #include "Regs.h"
 #include <unordered_set>
 
-namespace sys {
-namespace rv {
-    
+using namespace sys;
+using namespace sys::rv;
 
+namespace
+{
+
+class SpilledRdAttr : public AttrImpl<SpilledRdAttr, RVLINE + 2097152> {
+public:
+    bool fp;
+    int offset;
+    Op* ref;
+
+    SpilledRdAttr(bool fp, int offset, Op* ref) : fp(fp), offset(offset), ref(ref) {}
+
+    std::string toString() override {
+        return "<rd-spilled = " + std::to_string(offset) + (fp ? "f" : "") + ">";
+    }
+    SpilledRdAttr* clone() override { return new SpilledRdAttr(fp, offset, ref); }
+};
+
+class SpilledRsAttr : public AttrImpl<SpilledRsAttr, RVLINE + 2097152> {
+public:
+    bool fp;
+    int offset;
+    Op* ref;
+
+    SpilledRsAttr(bool fp, int offset, Op* ref) : fp(fp), offset(offset), ref(ref) {}
+
+    std::string toString() override {
+        return "<rs-spilled = " + std::to_string(offset) + (fp ? "f" : "") + ">";
+    }
+    SpilledRsAttr* clone() override { return new SpilledRsAttr(fp, offset, ref); }
+};
+
+class SpilledRs2Attr : public AttrImpl<SpilledRs2Attr, RVLINE + 2097152> {
+public:
+    bool fp;
+    int offset;
+    Op* ref;
+
+    SpilledRs2Attr(bool fp, int offset, Op* ref) : fp(fp), offset(offset), ref(ref) {}
+
+    std::string toString() override {
+        return "<rs2-spilled = " + std::to_string(offset) + (fp ? "f" : "") + ">";  
+    }
+    SpilledRs2Attr* clone() override { return new SpilledRs2Attr(fp, offset, ref); }
+};
+    
+} // namespace
+
+std::map<std::string, int> RegAlloc::stats() {
+    return { 
+        {"spilled", spilled}, 
+        {"convertedTotal", convertedTotal}
+    };
+}
+
+// Implemented in OpBase.cpp.
+std::string getValueNumber(Value value);
+
+void RegAlloc::runImpl(Region *region, bool isLeaf) {
     
 }
+
+void RegAlloc::run() {
+    
 }
+
+
 
--- rvcp/src/rv/RvPasses.h
+++ rvcp/src/rv/RvPasses.h
@@ -44,6 +44,23 @@ public:
     void run() override;
 };
 
+class RegAlloc : public Pass {
+    int spilled = 0;
+    int convertedTotal = 0;
+
+    std::map<FuncOp*, std::set<Reg>> usedRegisters;
+    std::map<std::string, FuncOp*> fnMap;
+
+    void runImpl(Region *region, bool isLeaf);
+    // Create both Prologue and Epilogue of a function.
+    void proEpilogue(FuncOp *fn, bool isLeaf);
+public:
+    RegAlloc(ModuleOp *module) : Pass(module) {}
+    std::string name() override { return "rv-regalloc"; }
+    std::map<std::string, int> stats() override;
+    void run() override;
+};
+
 // Dumps the output.
 class Dump : public Pass {
     std::string out;
